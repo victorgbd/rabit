6b78333425187fc819ce87a19d5d826cb0259c55
victor jose <victorjosegonzalez9@gmail.com>
2024-04-14 18:54:53 -0400

    ua

--- recibir/entrypoint.sh
+++ recibir/entrypoint.sh
@@ -1,2 +1,2 @@
 # Watch your .go files and invoke go build if the files changed.
-CompileDaemon --build="go build -o main main.go"  --command=./main
+CompileDaemon --build="go build -o recibir recibir.go"  --command=./main
